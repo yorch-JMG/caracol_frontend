a583a352308ccb61bdf9183fd456cf5f3bd39427
Fac <oakage132@gmail.com>
2021-12-02 21:24:55 -0800

    cambios homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,87 +9,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle style=
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('Fondocaracol'),
-              fit: BoxFit.cover
-            ),
-          ),
-          child: Scaffold(
-            backgroundColor: Colors.indigo,
-            body: Container(
-              padding: EdgeInsets.only(
-                  top: 130,
-
-                  bottom: 10,
-                  right: 10,
-                  left: 10
-              ),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT6q20JRkisLTEacLZZyWenViiAe60soW30kisUQhPwOryB2cxQlP7SX0zxj6tlfOJv1M&usqp=CAU"),
-                      alignment: Alignment.topCenter)),
-              child: Column(
+      title: ('Museo Caracol'),
+      home: Scaffold(appBar: AppBar(title: const Text('Museo Caracol'),
+          backgroundColor: Colors.pink,
+     ),
+          body:
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.all(10),
-                            child: RaisedButton(
-                              color: Colors.white,
-                              shape: new RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              onPressed: () {},
-                              child: SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: Center(
-                                  child: Text("OPTION 1",
-                                      textAlign: TextAlign.center),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.all(10),
-                            child: RaisedButton(
-                              color: Colors.white,
-                              shape: new RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0)),
-                              onPressed: () {},
-                              child: SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: Center(
-                                  child: Text("OPTION 2",
-                                      textAlign: TextAlign.center),
-                                ),
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
+                  ElevatedButton(
+                    style: style,
+                    onPressed: (){},
+                    child: const Text('Sexo')
                   ),
-
-                ],
+                  const SizedBox(height: 200),
+                  ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: const Text('Sexo 2')
+                  )
+              ],
               ),
-            ),
-          ),
-        ));
+      ),//const MyStatefulWidget()),
+    );
   }
 }
+
