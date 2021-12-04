482f89fde135bd33e049e37e1ca7edfed466ff76
Fac <oakage132@gmail.com>
2021-12-04 10:44:50 -0800

    ola

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -10,29 +11,69 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final ButtonStyle style=
-    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    ElevatedButton.styleFrom(
+        textStyle: const TextStyle(fontSize: 16),
+        fixedSize: const Size(140,50),
+        primary: Colors.blueAccent,
+        shape: RoundedRectangleBorder(
+        borderRadius:  BorderRadius.circular(50)
+    )
+    );
     return MaterialApp(
       title: ('Museo Caracol'),
-      home: Scaffold(appBar: AppBar(title: const Text('Museo Caracol'),
-          backgroundColor: Colors.pink,
+      home: Container(
+        decoration:  BoxDecoration(
+            image:  DecorationImage(
+                image: AssetImage("images/background.jpg"),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(title: const Text('Museo Caracol'),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
      ),
-          body:
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  ElevatedButton(
-                    style: style,
-                    onPressed: (){},
-                    child: const Text('Sexo')
-                  ),
-                  const SizedBox(height: 200),
-                  ElevatedButton(
-                    style: style,
-                    onPressed: () {},
-                    child: const Text('Sexo 2')
-                  )
-              ],
-              ),
+            body: Container(
+                child:Column(
+                  children: <Widget>[
+                    Image.asset('images/logo.png', width:300),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                      ElevatedButton(
+                        style: style,
+                          onPressed: (){},
+                          child: const Text('Eventos')
+                      ),
+                      const SizedBox(height: 200),
+                      ElevatedButton(
+                        style: style,
+                        onPressed: () {},
+                        child: const Text('Comprar boletos'),
+                      ),
+                      ]
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          ElevatedButton(
+                            style: style,
+                              onPressed: (){},
+                              child: const Text('Stats')
+                          ),
+                          const SizedBox(height: 00),
+                          ElevatedButton(
+                            style: style,
+                            onPressed: () {},
+                            child: const Text('?'),
+                          ),
+                        ]
+                    )
+                ],
+                ),
+          )
+        ),
       ),//const MyStatefulWidget()),
     );
   }
