a56cad7995677b6a7b8f7b238e6e403faf088a8a
Jorge <jorgeMG@jorgemdev.com>
2021-12-04 18:32:45 -0800

    onBoard slider implementado, necesita styling

---
+++ lib/on_boarding_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:caracol_frontend/home_page.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => SafeArea(
+      child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+                title: "Descubre",
+                body: "dsdnjdkndjnknkj"
+            ),
+            PageViewModel(
+                title: "Descubre",
+                body: "dsdnjdkndjnknkj"
+            )
+          ],
+          done: Text('Saltar'),
+          next: Text('Siguiente'),
+          onDone: () {
+            Navigator.of(context)
+                .pushReplacement(
+                    MaterialPageRoute(builder: (context) => HomePage())
+                );
+          },
+      )
+  );
+}
