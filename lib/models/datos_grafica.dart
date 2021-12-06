f660a80bcc809a2f788f2e26232505e0c62ca883
Jorge <jorgeMG@jorgemdev.com>
2021-12-05 21:51:41 -0800

    eventos page con las tarjetas de los eventos presionables

--- lib/models/datos_grafica.dart
+++ lib/models/datos_grafica.dart
@@ -1,10 +1,13 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+
 class DatosGrafica {
   int? anios;
   int? ventas;
+  charts.Color? color;
 
   factory DatosGrafica.fromJson(Map<String, dynamic> datos) {
     return DatosGrafica(datos["años"] as int, 
-                    datos['ventas'] as int); 
+                    datos['ventas'] as int);
   }
 
   DatosGrafica( int anios,  int ventas)  {
