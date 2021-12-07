8b69430c5a5ba5db525881a62341be00592fa063
Jorge <jorgeMG@jorgemdev.com>
2021-12-06 21:32:10 -0800

    Evento cliente actualizado

--- lib/evento_cliente_page.dart
+++ lib/evento_cliente_page.dart
@@ -13,32 +13,122 @@ class EventoClientePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(data.nombre != null){
-      return Scaffold(
-          appBar: AppBar(
-              title: Text(data.nombre.toString()),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(12.0),
-          alignment: Alignment.center,
-          child: Column(
+      return MaterialApp(
+        title: ("Museo Caracol"),
+        home: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/background.jpg"),
+                  fit: BoxFit.cover)),
+         
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              title: const Text('Museo Caracol'),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+          body: Column(
             children: <Widget>[
               Container(
-                height: 54.0,
+                height: 499,
                 padding: EdgeInsets.all(12.0),
-                child: Text("Descubre este evento:",
-                 style: TextStyle(fontWeight: FontWeight.w700))),
-              Text(data.asistentes.toString()),
-              Text(data.descripcion.toString()),
-              Text(data.fecha.toString()),
-              ElevatedButton(
-                  onPressed: () =>{
-                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => SalePage(noEvento : data.getNoEvento())))
+                child: Column(
+                    children:[
+                      Padding(
+                          padding: EdgeInsets.fromLTRB(12, 0, 0, 12),
+                          child: 
+
+                            Text("Descubre este evento:",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20
+                                ))),
+
+                      Padding(
+                          padding: EdgeInsets.fromLTRB(12, 0, 0, 12),
+                          child: 
+                            Text(data.nombre.toString(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 20
+                                ))),
+
+                          
+                      Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: 
+                            Text("Asistentes:",
+                                style: TextStyle(
+                                color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                )),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: 
+                            Text(data.asistentes.toString(),
+                                style: TextStyle(
+                                color: Colors.white,
+                                ) 
+                      )),
+                      Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: 
+                            Text("Descripcion:",
+                                style: TextStyle(
+                                color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                )),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: 
+                            Text(data.descripcion.toString(),
+                                style: TextStyle(
+                                color: Colors.white,
+                                ) 
+                                ,),
+                          
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: 
+                            Text("Fecha:",
+                                style: TextStyle(
+                                color: Colors.white,
+                                    fontWeight: FontWeight.bold
+                                )),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child: 
+                            Text(data.fecha.toString(),
+                                style: TextStyle(
+                                color: Colors.white,
+                                ) 
+                      )),
+                      Padding(
+                          padding: EdgeInsets.all(12.0),
+                          child:
+                            ElevatedButton(
+                              onPressed: () =>{
+                                Navigator.of(context).push(MaterialPageRoute(builder: (context) => SalePage(noEvento : data.getNoEvento())))
                   },
-                  child: Text("Consigue tu boleto"))
+                              child: Text("Consigue tu boleto"))
+
+                      )
+                    ]
+                )
+                
+                  
+              ),
             ],
           ),
         ),
-      );
+      ));
     }
     else{
       return Scaffold(
